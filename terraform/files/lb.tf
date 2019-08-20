d2ae9723eade4de4e9b7dc18016e48a5c156169d
Lada <kalinkina@tlpt.ru>
2019-08-20 17:22:31 +0300

    Terraform-2

--- terraform/files/lb.tf
+++ terraform/files/lb.tf
@@ -1,18 +1,18 @@
 resource "google_compute_forwarding_rule" "default" {
-  name = "http-lb"
-  region = "${var.region}"
-  target = "${google_compute_target_pool.default.self_link}"
+  name                  = "http-lb"
+  region                = "${var.region}"
+  target                = "${google_compute_target_pool.default.self_link}"
   load_balancing_scheme = "EXTERNAL"
-  port_range  = "9292"
+  port_range            = "9292"
 }
 
 resource "google_compute_target_pool" "default" {
-  name = "reddit-pool"
-  region  = "${var.region}"
+  name             = "reddit-pool"
+  region           = "${var.region}"
   session_affinity = "NONE"
 
   instances = [
-    "europe-west1-b/reddit-app"
+    "europe-west1-b/reddit-app",
   ]
 
   health_checks = [
@@ -21,7 +21,7 @@ resource "google_compute_target_pool" "default" {
 }
 
 resource "google_compute_http_health_check" "default" {
-  name = "check-reddit-app"
+  name         = "check-reddit-app"
   request_path = "/"
-  port  = "9292"
+  port         = "9292"
 }
---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,12 @@
+variable public_key_path {
+  description = "Path to the public key used to connect to instance"
+}
+
+variable zone {
+  description = "zone"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-base-app"
+}
