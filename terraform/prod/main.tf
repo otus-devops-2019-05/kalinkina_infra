f3e4fda677357ed806817fbd37391a4b569aba02
Lada <kalinkina@tlpt.ru>
2019-08-28 00:32:45 +0300

    Main homework

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -6,27 +6,43 @@ terraform {
 provider "google" {
   # Версия провайдера
   version = "2.0.0"
-
-  # ID проекта
   project = "${var.project}"
-  region  = "${var.region}"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+  alias   = "app"
+}
+
+provider "google" {
+  # Версия провайдера
+  version = "2.0.0"
+  project = "${var.project}"
+  region  = "europe-west4"
+  zone    = "europe-west4-c"
+  alias   = "db"
 }
 
 module "app" {
   source          = "../modules/app"
   public_key_path = "${var.public_key_path}"
-  zone            = "${var.zone}"
   app_disk_image  = "${var.app_disk_image}"
+  providers = {
+    google = "google.app"
+  }
 }
 
 module "db" {
   source          = "../modules/db"
   public_key_path = "${var.public_key_path}"
-  zone            = "${var.zone}"
   db_disk_image   = "${var.db_disk_image}"
+  providers = {
+    google = "google.db"
+  }
 }
 
 module "vpc" {
   source        = "../modules/vpc"
   source_ranges = ["196.245.151.77/22"]
+  providers = {
+    google = "google.app"
+  }
 }
--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -2,13 +2,6 @@ variable project {
   description = "Project ID"
 }
 
-variable region {
-  description = "Region"
-
-  # Значение по умолчанию
-  default = "europe-west1"
-}
-
 variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
@@ -20,10 +13,6 @@ variable disk_image {
 
 variable private_key {
   description = "Path to the private key used for ssh access"
-}
-
-variable zone {
-  description = "zone"
 }
 
 variable "app_count" {
